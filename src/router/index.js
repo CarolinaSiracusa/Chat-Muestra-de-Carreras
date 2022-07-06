1ae6272e607849a0dc3eac2c5542c4d584305ba6
Carolina Siracusa <54592916+CarolinaSiracusa@users.noreply.github.com>
2022-07-06 17:06:07 -0300

    Rutas Rama1

--- src/router/index.js
+++ src/router/index.js
@@ -6,51 +6,27 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/geografia',
-    name: 'Chat-Geografia',
-    props: { department: "GEOGRAFIA-TURISMO", name: "Geografía y Turismo" },
-    component: Home
-  },
-  {
-      path: '/matematica',
-      name: 'Chat-Matematica',
-      props: { department: "Departamento-Matematica", name: "Matemática" },
-      component: Home
-    },
-  {
-    path: '/sgbu',
-    name: 'Chat-Bienestar-Universitario',
-    props: { department: "Secretaria-General-Bienestar-Universitario", name: "Secretaría General de Bienestar Universitario" },
+    path: '/doe',
+    name: 'Chat-DOE',
+    props: { department: "DOE", name: "Departamento de Orientación Educacional" },
     component: Home
     },
     {
-      path: '/pens',
-      name: 'Chat-Profesorados-Escuela-Normal-Superior',
-      props: { department: "Profesorados-Escuela-Normal-Superior", name: "Profesorados Escuela Normal Superior" },
-      component: Home
-      },
-    {
-    path: '/agronomia',
-    name: 'Chat-Agronomia',
-    props: { department: "AGRONOMIA", name: "Agronomía" },
-    component: Home
-    },
-    {
-      path: '/biologia',
-      name: 'Chat-Biologia',
-      props: { department: "BIOLOGIA", name: "Biologia, Bioquimica y Farmacia" },
+      path: '/juan23',
+      name: 'Chat-Juan',
+      props: { department: "JUAN-XXIII", name: "Juan XXIII" },
       component: Home
     },
     {
-      path: '/administracion',
-      name: 'Chat-Administracion',
-      props: { department: "CIENCIAS-DE-LA-ADMINISTRACION", name: "Ciencias de la Administración" },
+      path: '/geologia',
+      name: 'Chat-Geologia',
+      props: { department: "GEOLOGIA", name: "Geología" },
       component: Home
     },
     {
-      path: '/isft',
-      name: 'Chat-ISFT',
-      props: { department: "INSTITUTO-SUPERIOR-FORMACION-TECNICA", name: "Instituto Superior de Formación Técnica Nª191" },
+      path: '/ucalp', 
+      name: 'Chat-UCALP',
+      props: { department: "UNIVERSIDAD-CATOLICA-DE-LA-PLATA", name: "Universidad Católica de la Plata" },
       component: Home
     },
     {
@@ -58,32 +34,7 @@ const routes = [
       name: 'Chat-SPEC',
       props: { department: "SECRETARIA-POSGRADO-EDUCACION-CONTINUA", name: "Secretaria de Posgrado y Eduación Continua" },
       component: Home
-    },
-    {
-      path: '/comercio',
-      name: 'Chat-Comercio',
-      props: { department: "ESCUELA-DE-COMERCIO", name: "Escuela Superior de Comercio" },
-      component: Home
-    },
-    {
-      path: '/salud',
-      name: 'Chat-Salud',
-      props: { department: "CIENCIAS-DE-LA-SALUD", name: "Ciencias de la Salud" },
-      component: Home
-    },
-    {
-      path: '/educacion',
-      name: 'Chat-Educacion',
-      props: { department: "CIENCIAS-DE-LA-EDUCACION", name: "Ciencias de la Educación" },
-      component: Home
-    },
-    {
-      path: '/unsregion',
-      name: 'Chat-UNS-Region',
-      props: { department: "UNS-EN-LA-REGION", name: "UNS en la Región" },
-      component: Home
-    },
-    
+    }
     ]
 
     const router = new VueRouter({
